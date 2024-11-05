7679462f8b2f5bca4a4135964e2a5f0fb36f69c9
kakun45 <email4blackkat@gmail.com>
2024-11-04 19:20:30 -0500

    Initial Commit

---
+++ xeniya_gothic_times_table/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  xeniya_gothic_times_table
+//
+//  Created by XS on 11/1/24.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet var a_var: UILabel!
+    @IBOutlet var b_var: UILabel!
+    
+    // Get User Input:
+    @IBOutlet var userGuess: UITextField!
+    // info textLabel
+    @IBOutlet var inputInfoLabel: UILabel!
+    
+    var currentA: Int  = 0
+    var currentB: Int  = 0
+    var currentResult: Int  = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        generateProblem()
+    }
+    
+    private func generateProblem() {
+        currentA = randomNum()
+        currentB = randomNum()
+        a_var.text = String(currentA)
+        b_var.text = String(currentB)
+        currentResult = currentA * currentB
+        print("mult res: \(currentResult)")
+    }
+    
+    @IBAction func userChoiceBtn(_ sender: UIButton) {
+        // Get user input from text field
+        guard let userInput = userGuess.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let userInpNumber = Int(userInput) else {
+            inputInfoLabel.text = "Please, enter a valid number."
+            
+            userGuess.text = ""
+            // Hide the info msg after 3sec delay
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        self.inputInfoLabel.text = ""
+                    }
+                                          
+            return
+        }
+        
+        if currentResult == userInpNumber {
+//            resultLabel.text = "Your guess: \(userInpNumber) is correct"
+//            resultLabel.textColor = .green
+            print("Your guess: \(userInpNumber) is correct")
+        } else {
+//            resultLabel.text = "Your guess: \(userInpNumber) is INcorrect"
+//            resultLabel.textColor = .red
+            print("!Your guess: \(String(describing: userInpNumber)) is INcorrect! currentResult: \(currentResult)")
+        }
+        // Clear the text field & reset a problem
+        userGuess.text = ""
+        generateProblem()
+    }
+    
+}
+
---
+++ xeniya_gothic_times_table/variable.swift
@@ -0,0 +1,17 @@
+//
+//  variable.swift
+//  xeniya_gothic_times_table
+//
+//  Created by XS on 11/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+func randomNum() -> Int {
+    return Int.random(in: 1...12)
+}
+
+// FFE762 numers color: not used
+let yellowColor = Color(red: 1, green: 0.9058823529411765, blue: 0.3843137254901961) // RGB values between 0.0 and 1.0
